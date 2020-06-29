826c0cad3c2646ddefd44c870bc4bbff465b6b08
rkparida7 <rkparida007@gmail.com>
2020-06-30 01:21:32 +0530

    Test2test2

---
+++ src/Testing1/HelloWorld.java
@@ -0,0 +1,13 @@
+package Testing1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("*****");
+		System.out.println("Build Running");
+		System.out.println("Build End");
+		System.out.println("*=============*");
+	}
+
+}
